0440ee33fe84ebb2d581dc464fcb48cb31975671
SkippoP <157710600+Skipperii1@users.noreply.github.com>
2024-06-03 13:57:22 +0200

    Update python_example.py

--- examples/solana-wallet/generate-secret-phrase/python_example.py
+++ examples/solana-wallet/generate-secret-phrase/python_example.py
@@ -1,18 +1,16 @@
+import theblockchainapi
 from theblockchainapi import SolanaAPIResource, SolanaWallet
 
 # Get an API key pair for free here: https://dashboard.blockchainapi.com/api-keys
-MY_API_KEY_ID = None
-MY_API_SECRET_KEY = None
+MY_API_KEY_JD = " wjFnStWua47Svft "
+MY_API_SECRET_KEY = " 8BsxgyatFzKjTXzb "@@///
 
 
 BLOCKCHAIN_API_RESOURCE = SolanaAPIResource(
-    api_key_id=MY_API_KEY_ID,
-    api_secret_key=MY_API_SECRET_KEY
+    public _key = resource.derive_public_key
+    secret_key = reaource.generate_secret_key(
 )
 
-
-def example():
-    try:
         assert MY_API_KEY_ID is not None
         assert MY_API_SECRET_KEY is not None
     except AssertionError:
